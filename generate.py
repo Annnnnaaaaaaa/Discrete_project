0e171239306f090a33c28e439c888cb714698bf3
Annnnnaaaaaaa <dnipro.anna@gmail.com>
2025-12-13 16:03:49 +0200

    клас generate

--- generate.py
+++ generate.py
@@ -1,38 +1,43 @@
-from math import inf
 import random
 
-n = 10
-density = 0.8
 
-g_matrix = [[inf] * n for i in range(n)]
+class GraphGenerator:
+    """
+    Генератор простих неорієнтованих зважених графів.
+    """
 
-# lst = [True, False]
-# sm_weights = [density, 1 - density]
-# tot_edges_am = (n*(n-1))/2
-#
-# for i in range(int(tot_edges_am)):
-#     flg = random.choices(lst, weights=sm_weights)
-#     if flg:
-#         for k in range(n):
-#             for j in range(k + 1, n):
-#                 w = random.randint(1, 1300)
-#                 g_matrix[k][j] = w
-#                 g_matrix[j][k] = w
-#
-# print(g_matrix)
+    @staticmethod
+    def generate(n, density, min_weight=1, max_weight=100):
+        """
+        Генерує простий неорієнтований зважений граф заданої щільності.
 
+        Args:
+            n (int): кількість вершин
+            density (float): щільність графу (0 < density <= 1)
+            min_weight (float): мінімальна вага ребра
+            max_weight (float): максимальна вага ребра
 
-for k in range(n):
-    for j in range(k + 1, n):
+        Returns:
+            Graph: згенерований граф
+        """
+        graph = Graph(n)
 
-        # 1. Вирішуємо, чи має існувати ребро (k, j) з імовірністю 'density'
-        # random.random() повертає float у діапазоні [0.0, 1.0)
-        if random.random() < density:
-            # 2. Якщо ребро існує, призначаємо йому випадкову вагу
-            w = random.randint(1, 1300)
+        # Максимальна кількість ребер у простому неорієнтованому графі
+        max_edges = n * (n - 1) // 2
 
-            # 3. Заповнюємо матрицю суміжності (для неорієнтованого графа)
-            g_matrix[k][j] = w
-            g_matrix[j][k] = w
+        # Необхідна кількість ребер для заданої щільності
+        target_edges = int(max_edges * density)
 
-print(g_matrix)
+        # Створюємо список всіх можливих ребер
+        all_edges = [(i, j) for i in range(n) for j in range(i + 1, n)]
+
+        # Перемішуємо і беремо потрібну кількість
+        random.shuffle(all_edges)
+        selected_edges = all_edges[:target_edges]
+
+        # Додаємо вибрані ребра з випадковими вагами
+        for u, v in selected_edges:
+            weight = random.uniform(min_weight, max_weight)
+            graph.add_edge(u, v, weight)
+
+        return graph
