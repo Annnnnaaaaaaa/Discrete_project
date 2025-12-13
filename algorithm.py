28e41dac072fac4d3d062eb05527fc0b7e096423
vimajeu <vitalina.hatsura.25@kse.org.ua>
2025-12-13 17:18:53 +0200

    lll

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,27 @@
 from graph import Graph
+from generate import GraphGenerator
 from math import inf
 
 class Algorithm:
-    def __init__(self, n, density):
-        self.n = n
-        self.density = density
+    def __init__(self, g: Graph):
+        self.g = g
 
-    def hamilton(self):
+    def is_hamilton(self):
+        for v in range(self.g.n):
+            print(f"  Вершина {v}: ступінь = {self.g.degree(v)}")
+        num = 0
+        while num < self.g.n:
+            for v in range(self.g.n):
+                if self.g.degree(v) < self.g.n/2:
+                    print("Це не Гамільтонів граф.")
+                    return False
+                else:
+                    num += 1
+                    continue
 
 
 
 
-
+sm_g = GraphGenerator.generate(10, 0.9, min_weight=10, max_weight=100)
+a = Algorithm(sm_g)
+a.is_hamilton()
