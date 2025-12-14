f7f000adffacf2a1dabd461e3fd9749136349a4c
Annnnnaaaaaaa <dnipro.anna@gmail.com>
2025-12-14 04:33:40 +0200

    няяняняня

--- graph.py
+++ graph.py
@@ -1,15 +1,15 @@
-class Graph:
-    """Повний неорієнтований зважений граф."""
-
-    def __init__(self, n):
-        self.n = n
-        self.weights = {}
-
-    def add_edge(self, u, v, weight):
-        self.weights[(min(u, v), max(u, v))] = weight
-
-    def get_weight(self, u, v):
-        return self.weights[(min(u, v), max(u, v))]
-
-    def get_all_edges(self):
-        return self.weights
+# class Graph:
+#     """Повний неорієнтований зважений граф."""
+#
+#     def __init__(self, n):
+#         self.n = n
+#         self.weights = {}
+#
+#     def add_edge(self, u, v, weight):
+#         self.weights[(min(u, v), max(u, v))] = weight
+#
+#     def get_weight(self, u, v):
+#         return self.weights[(min(u, v), max(u, v))]
+#
+#     def get_all_edges(self):
+#         return self.weights
